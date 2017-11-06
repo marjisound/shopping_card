d34731c4d124f95bdf9b90bc19591a3bbbb0dac5
marji_sound <marji_sound@yahoo.com>
2017-11-06 21:41:33 +0000

    Commiting the project

---
+++ ShoppingKart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShoppingKart.Startup))]
+namespace ShoppingKart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
